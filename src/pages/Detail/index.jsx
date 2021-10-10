578a3786be7e0dd9aaff03cf61794b02490b3831
Mainak Das <mainakdas104@gmail.com>
2021-10-10 15:37:26 +0530

    Overhauled movie detail page
    Improved redux functions
    - All logics are handled by redux
    - Removed runtime fetch from SimilarCard to redux
    - Removed swr from files
    - Destructured props

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,98 +1,102 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import {
-	fetcher,
-	filterCast,
-	filterDirector,
-	getGenres,
-	getYear,
-	runtimeConverter,
-} from '../../libs/helperFuntions';
-import tmdbInstance from '../../libs/tmdbInstance';
+  filterCast,
+  filterDirector,
+  getGenres,
+  getYear,
+  runtimeConverter,
+} from '../../utils/helperFuntions';
 import Loader from '../../components/Loader';
-import useSWR from 'swr';
-import apiEndpoints from '../../libs/apiEndpoints';
 import SimilarCard from '../../components/SimilarCard';
 import DetailsInfo from '../../components/DetailsInfo';
+import {
+  fetchFullMovie,
+  resetMovie,
+} from '../../redux/slice/movieDetailsSlice';
+import { useSelector } from 'react-redux';
 
-const ComponentName = (props) => {
-	const { data, error } = useSWR(
-		apiEndpoints.movieDetails(props.match.params.id),
-		fetcher
-	);
-	const [crews, setCrews] = useState([]);
-	const [similar, setSimilar] = useState([]);
-	const dispatch = useDispatch();
-	useEffect(() => {
-		tmdbInstance
-			.get(apiEndpoints.crewsAndCasts(props.match.params.id))
-			.then((res) => setCrews(res.data));
-		tmdbInstance
-			.get(apiEndpoints.similarMovies(props.match.params.id))
-			.then((res) => setSimilar(res.data.results));
-	}, [dispatch, props.match.params.id]);
+const ComponentName = ({ match }) => {
+  const dispatch = useDispatch();
 
-	//TODOimplement error handling
+  useEffect(() => {
+    dispatch(fetchFullMovie(match.params.id));
 
-	if (!data && !error) {
-		return <Loader />;
-	}
-	return (
-		<div className='flex flex-col gap-8 pb-10'>
-			<div className='relative'>
-				<img
-					src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}
-					alt={data.title}
-					className='w-full'
-				/>
-				<h1 className='absolute font-bold break-words text-7xl bottom-96 left-32 text-netflixwhite'>
-					{data.title}
-				</h1>
-			</div>
-			<div className='flex flex-col gap-4 px-32'>
-				<div className='grid grid-cols-2 gap-32 '>
-					<div className='flex flex-col gap-4'>
-						<div className='flex items-center gap-8 '>
-							<span>{getYear(data.release_date)}</span>
-							<span>{runtimeConverter(data.runtime)}</span>
-						</div>
-						<p className='text-lg font-medium'>{data.overview}</p>
-					</div>
-					<div className='flex flex-col gap-4'>
-						<DetailsInfo title='Cast' detail={filterCast(crews.cast)} />
-						<DetailsInfo title='Genres' detail={getGenres(data.genres)} />
-					</div>
-				</div>
-				<div className='flex flex-col gap-4'>
-					<h3 className='text-2xl font-bold'>More Like This</h3>
-					<div className='grid content-center grid-cols-5 gap-8 justify-items-center'>
-						{similar
-							.filter((backdropCheck) => backdropCheck.backdrop_path !== null)
-							.filter((_, idx) => idx < 15)
-							.map((similarMovieData) => (
-								<SimilarCard
-									id={similarMovieData.id}
-									key={similarMovieData.id}
-									overview={similarMovieData.overview}
-									imageUrl={similarMovieData.backdrop_path}
-									release={getYear(similarMovieData.release_date)}
-								/>
-							))}
-					</div>
-				</div>
-				<hr />
-				<div className='flex flex-col gap-6 '>
-					<h3 className='text-2xl'>
-						About <strong>{data.title}</strong>
-					</h3>
-					<div className='flex flex-col w-1/2 gap-4'>
-						<DetailsInfo title='Director' detail={filterDirector(crews.crew)} />
-						<DetailsInfo title='Cast' detail={filterCast(crews.cast)} />
-						<DetailsInfo title='Genres' detail={getGenres(data.genres)} />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    return () => dispatch(resetMovie());
+  }, [dispatch, match.params.id]);
+
+  const { uiState, movie, error } = useSelector((state) => state.movieDetails);
+
+  return (
+    <>
+      {uiState === 'FAILED' && (
+        <div>{error.message || 'Something went wrong'}</div>
+      )}
+      {uiState === 'LOADING' && <Loader />}
+      {uiState === 'DONE' && (
+        <div className='flex flex-col gap-8 pb-10'>
+          <div className='relative'>
+            <img
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt={movie.title}
+              className='w-full'
+            />
+            <h1 className='absolute font-bold break-words text-7xl bottom-96 left-32 text-netflixwhite'>
+              {movie.title}
+            </h1>
+          </div>
+          <div className='flex flex-col gap-4 px-32'>
+            <div className='grid grid-cols-2 gap-32 '>
+              <div className='flex flex-col gap-4'>
+                <div className='flex items-center gap-8 '>
+                  <span>{getYear(movie.release_date)}</span>
+                  <span>{runtimeConverter(movie.runtime)}</span>
+                </div>
+                <p className='text-lg font-medium'>{movie.overview}</p>
+              </div>
+              <div className='flex flex-col gap-4'>
+                <DetailsInfo title='Cast' detail={filterCast(movie.cast)} />
+                <DetailsInfo title='Genres' detail={getGenres(movie.genres)} />
+              </div>
+            </div>
+            <div className='flex flex-col gap-4'>
+              <h3 className='text-2xl font-bold'>More Like This</h3>
+              <div className='grid content-center grid-cols-5 gap-8 justify-items-center'>
+                {movie.similar
+                  .filter((_, idx) => idx < 15)
+                  .filter(
+                    (backdropCheck) => backdropCheck.backdrop_path !== null
+                  )
+                  .map((similarMovieData) => (
+                    <SimilarCard
+                      id={similarMovieData.id}
+                      key={similarMovieData.id}
+                      overview={similarMovieData.overview}
+                      imageUrl={similarMovieData.backdrop_path}
+                      release={similarMovieData.release_date}
+                      runtime={similarMovieData.runtime}
+                    />
+                  ))}
+              </div>
+            </div>
+            <hr />
+            <div className='flex flex-col gap-6 '>
+              <h3 className='text-2xl'>
+                About <strong>{movie.title}</strong>
+              </h3>
+              <div className='flex flex-col w-1/2 gap-4'>
+                <DetailsInfo
+                  title='Director'
+                  detail={filterDirector(movie.crew)}
+                />
+                <DetailsInfo title='Cast' detail={filterCast(movie.cast)} />
+                <DetailsInfo title='Genres' detail={getGenres(movie.genres)} />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 export default ComponentName;
