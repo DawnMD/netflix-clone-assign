7ecedb98f63c22fcecd6b3aa6bd80a4c9622a9b8
Mainak Das <mainakdas104@gmail.com>
2021-10-10 14:59:22 +0530

    Improved and added funtions
    Added funtions for better management
    - API success
    - API pending
    - API failed
    - reset

--- src/redux/slice/movieDetailsSlice.js
+++ src/redux/slice/movieDetailsSlice.js
@@ -4,6 +4,7 @@ import {
   fetchSimilarMovies,
   fetchMovieCastCrew,
 } from '../../apis/movieEndpoints';
+import { getAllDetails } from '../../utils/helperFuntions';
 
 export const fetchFullMovie = createAsyncThunk(
   'movieDetails/fetch',
@@ -16,7 +17,7 @@ export const fetchFullMovie = createAsyncThunk(
       return {
         ...movie,
         ...castAndCrew,
-        similar: similarMovies.results,
+        similar: await getAllDetails(similarMovies.results),
       };
     } catch (error) {
       return error.response;
@@ -33,6 +34,26 @@ const initialState = {
 const movieDetailsSlice = createSlice({
   name: 'movieDetails',
   initialState,
+  reducers: {
+    resetMovie: (state, action) => {
+      return initialState;
+    },
+  },
+  extraReducers: {
+    [fetchFullMovie.rejected]: (state, action) => {
+      state.uiState = 'FAILED';
+      state.error = action.payload;
+    },
+    [fetchFullMovie.pending]: (state, action) => {
+      state.uiState = 'LOADING';
+    },
+    [fetchFullMovie.fulfilled]: (state, action) => {
+      state.uiState = 'DONE';
+      state.movie = action.payload;
+    },
+  },
 });
+
+export const { resetMovie } = movieDetailsSlice.actions;
 
 export default movieDetailsSlice.reducer;
