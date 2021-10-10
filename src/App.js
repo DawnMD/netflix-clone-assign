7df75357bfaa3cded3171a6863c5253821962115
Mainak Das <mainakdas104@gmail.com>
2021-10-10 15:02:05 +0530

    Overhauled different funtions
    Improved page load and separation of concerns
    - No longer fetch all movies during all start at App.js
    - Removed unnecessary dispatchs

--- src/App.js
+++ src/App.js
@@ -2,37 +2,24 @@ import Header from './components/Header';
 import Movies from './pages/Movies';
 import Detail from './pages/Detail';
 import { Redirect, Route, Switch } from 'react-router';
-import { useDispatch } from 'react-redux';
-import {
-	fetchDiscoverMovies,
-	fetchPopularMovies,
-	fetchTopRatedMovies,
-} from './redux/slice/movieSlice';
-import { useEffect } from 'react';
 import ScrollToTop from './components/HOC/ScrollToTop';
 
 function App() {
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(fetchDiscoverMovies());
-		dispatch(fetchPopularMovies());
-		dispatch(fetchTopRatedMovies());
-	}, [dispatch]);
-	return (
-		<div className='flex flex-col'>
-			<Header />
-			<ScrollToTop />
-			<Switch>
-				{/* switching to movies page on load */}
-				<Route exact path='/'>
-					<Redirect to='/movies' />
-				</Route>
-				{/* actual movies page */}
-				<Route exact path='/movies' component={Movies} />
-				<Route path='/movies/:id' component={Detail} />
-			</Switch>
-		</div>
-	);
+  return (
+    <div className='flex flex-col'>
+      <Header />
+      <ScrollToTop />
+      <Switch>
+        {/* switching to movies page on load */}
+        <Route exact path='/'>
+          <Redirect to='/movies' />
+        </Route>
+        {/* actual movies page */}
+        <Route exact path='/movies' component={Movies} />
+        {/* <Route path='/movies/:id' component={Detail} /> */}
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
