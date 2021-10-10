95c5a5a3090e6641f56db434ded8fc8f6739fb2f
Mainak Das <mainakdas104@gmail.com>
2021-10-10 15:37:05 +0530

    Destructured props for better intellisense

--- src/components/ScrollWrapper/index.jsx
+++ src/components/ScrollWrapper/index.jsx
@@ -1,5 +1,5 @@
 import classes from './scrollwrapper.module.css';
-const ScrollWrapper = (props) => {
-	return <div className={`flex gap-2 ${classes.scroll}`}>{props.children}</div>;
+const ScrollWrapper = ({ children }) => {
+  return <div className={`flex gap-2 ${classes.scroll}`}>{children}</div>;
 };
 export default ScrollWrapper;
