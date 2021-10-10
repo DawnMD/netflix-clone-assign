3c4d6b8ed21d0f0ee8e65fb8c2e1bff89c776e39
Mainak Das <mainakdas104@gmail.com>
2021-10-10 15:04:09 +0530

    Improved all movies page
    Added different states for movies
    - Page renders based on uiState from redux store
    - Handled error
    - fetch all movies during component mount
    - reset to initial state during unmount

--- src/pages/Movies/index.jsx
+++ src/pages/Movies/index.jsx
@@ -3,65 +3,82 @@ import ItemCard from '../../components/ItemCard';
 import { useSelector } from 'react-redux';
 import Loader from '../../components/Loader';
 import ScrollWrapper from '../../components/ScrollWrapper';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import {
+  fetchAllMovies,
+  resetAllMovies,
+} from '../../redux/slice/allMovieSlice';
 
 const Movies = () => {
-	const discoverMovies = useSelector((state) => state.movies.discover);
-	const topRatedMovies = useSelector((state) => state.movies.topRated);
-	const popularMovies = useSelector((state) => state.movies.popular);
-	return (
-		<>
-			{discoverMovies && topRatedMovies && popularMovies ? (
-				<div className='flex flex-col gap-6 pb-10 pl-20'>
-					<SectionList title='Discover Movies'>
-						<ScrollWrapper>
-							{discoverMovies &&
-								discoverMovies.map((movie) => (
-									<ItemCard
-										id={movie.id}
-										key={movie.id}
-										title={movie.title}
-										imageUrl={movie.backdrop_path}
-										description={movie.overview}
-										release={movie.release_date}
-									/>
-								))}
-						</ScrollWrapper>
-					</SectionList>
-					<SectionList title='Top Rated Movies'>
-						<ScrollWrapper>
-							{topRatedMovies &&
-								topRatedMovies.map((movie) => (
-									<ItemCard
-										id={movie.id}
-										key={movie.id}
-										title={movie.title}
-										imageUrl={movie.backdrop_path}
-										description={movie.overview}
-										release={movie.release_date}
-									/>
-								))}
-						</ScrollWrapper>
-					</SectionList>
-					<SectionList title='Popular Movies'>
-						<ScrollWrapper>
-							{popularMovies &&
-								popularMovies.map((movie) => (
-									<ItemCard
-										id={movie.id}
-										key={movie.id}
-										title={movie.title}
-										imageUrl={movie.backdrop_path}
-										description={movie.overview}
-										release={movie.release_date}
-									/>
-								))}
-						</ScrollWrapper>
-					</SectionList>
-				</div>
-			) : (
-				<Loader />
-			)}
-		</>
-	);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllMovies());
+
+    return () => dispatch(resetAllMovies());
+  }, [dispatch]);
+
+  const { uiState, movies, error } = useSelector((state) => state.allMovies);
+
+  return (
+    <>
+      {uiState === 'FAILED' && (
+        <div>{error.message || 'Something went wrong'}</div>
+      )}
+      {uiState === 'LOADING' && <Loader />}
+      {uiState === 'DONE' &&
+        (!movies.discoverMovies.length &&
+        !movies.popularMovies.length &&
+        !movies.topRatedMovies.length ? (
+          <Loader />
+        ) : (
+          <div className='flex flex-col gap-6 pb-10 pl-20'>
+            <SectionList title='Discover Movies'>
+              <ScrollWrapper>
+                {movies.discoverMovies.map((movie) => (
+                  <ItemCard
+                    id={movie.id}
+                    key={movie.id}
+                    title={movie.title}
+                    imageUrl={movie.backdrop_path}
+                    description={movie.overview}
+                    release={movie.release_date}
+                  />
+                ))}
+              </ScrollWrapper>
+            </SectionList>
+            <SectionList title='Top Rated Movies'>
+              <ScrollWrapper>
+                {movies.topRatedMovies.map((movie) => (
+                  <ItemCard
+                    id={movie.id}
+                    key={movie.id}
+                    title={movie.title}
+                    imageUrl={movie.backdrop_path}
+                    description={movie.overview}
+                    release={movie.release_date}
+                  />
+                ))}
+              </ScrollWrapper>
+            </SectionList>
+            <SectionList title='Popular Movies'>
+              <ScrollWrapper>
+                {movies.popularMovies.map((movie) => (
+                  <ItemCard
+                    id={movie.id}
+                    key={movie.id}
+                    title={movie.title}
+                    imageUrl={movie.backdrop_path}
+                    description={movie.overview}
+                    release={movie.release_date}
+                  />
+                ))}
+              </ScrollWrapper>
+            </SectionList>
+          </div>
+        ))}
+    </>
+  );
 };
 export default Movies;
