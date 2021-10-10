4e10e0a3c7b13c027565c40d21098b3d56a2ad81
Mainak Das <mainakdas104@gmail.com>
2021-10-10 15:31:43 +0530

    Removed condition check from helpers

--- src/utils/helperFuntions.js
+++ src/utils/helperFuntions.js
@@ -1,4 +1,3 @@
-import tmdbInstance from '../libs/tmdbInstance';
 import { fetchFullMovieDetails } from '../apis/movieEndpoints';
 
 export const getYear = (dateString) => {
@@ -10,38 +9,21 @@ export const runtimeConverter = (runtime) => {
 };
 
 export const filterDirector = (data) => {
-  if (data) {
-    return data
-      .filter((director) => director.job === 'Director')
-      .map((director) => director.original_name)
-      .join(', ');
-  } else {
-    return null;
-  }
+  return data
+    .filter((director) => director.job === 'Director')
+    .map((director) => director.original_name)
+    .join(', ');
 };
 
 export const filterCast = (data) => {
-  if (data) {
-    return data
-      .filter((director) => director.known_for_department === 'Acting')
-      .map((director) => director.original_name)
-      .join(', ');
-  } else {
-    return null;
-  }
+  return data
+    .filter((director) => director.known_for_department === 'Acting')
+    .map((director) => director.original_name)
+    .join(', ');
 };
 
 export const getGenres = (data) => {
-  if (data) {
-    return data.map((genre) => genre.name).join(', ');
-  } else {
-    return null;
-  }
-};
-
-export const fetcher = async (url) => {
-  const { data } = await tmdbInstance.get(url);
-  return data;
+  return data.map((genre) => genre.name).join(', ');
 };
 
 export const getAllDetails = async (movieArray) =>
